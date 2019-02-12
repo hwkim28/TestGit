259fa5c2b6252d225beaae340ef6f508ebff21f5
hwkim28 <47454404+hwkim28@users.noreply.github.com>
2019-02-12 23:10:10 +0900

    Add files via upload

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -5,5 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test World");
 		System.out.println("Test 2월 12일");
-	}
+		System.out.println("succes");
+	}
+
+
 }
