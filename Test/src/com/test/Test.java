edebb221c744002c1dfca5c3c668ad25031b571e
KimHyeongWuk <giod28@nate.com>
2019-02-12 23:20:56 +0900

    add

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -3,8 +3,13 @@ package com.test;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Test World");
-		System.out.println("Test 2월 12일");
-		System.out.println("succes");
+	
+		int a[] = {5,2,3,6,5,8,4,9,};
+		int sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum+=a[i];
+		}
+		
+		System.out.println(sum);
 	}
 }
